69b5c3f0e49c878c2fdab9e9d2c766614665aea7
soasme <soasme@gmail.com>
2015-12-23 15:40:44 +0800

    Implement UserGroupPermission.delete_by_permission

--- flask_perm/services/user_group_permission.py
+++ flask_perm/services/user_group_permission.py
@@ -6,28 +6,36 @@ from ..core import db
 from ..models import UserGroupPermission
 
 def create(user_group_id, permission_id):
-    user_permission = UserGroupPermission(
+    user_group_permission = UserGroupPermission(
         user_group_id=user_group_id,
         permission_id=permission_id,
     )
-    db.session.add(user_permission)
+    db.session.add(user_group_permission)
     try:
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
-        user_permission = UserGroupPermission.query.filter_by(
+        user_group_permission = UserGroupPermission.query.filter_by(
             user_group_id=user_group_id,
             permission_id=permission_id,
         ).first()
-    return user_permission
+    return user_group_permission
 
 def delete(user_group_id, permission_id):
-    user_permission = UserGroupPermission.query.filter_by(
+    user_group_permission = UserGroupPermission.query.filter_by(
         user_group_id=user_group_id,
         permission_id=permission_id
     ).first()
-    if user_permission:
-        db.session.delete(user_permission)
+    if user_group_permission:
+        db.session.delete(user_group_permission)
+    db.session.commit()
+
+def delete_by_permission(permission_id):
+    user_group_permissions = UserGroupPermission.query.filter_by(
+        permission_id=permission_id
+    ).all()
+    for user_group_permission in user_group_permissions:
+        db.session.delete(user_group_permission)
     db.session.commit()
 
 def get_user_groups_by_permission(permission_id):
