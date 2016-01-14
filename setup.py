d428ed97c8994f9c6ec97ea2024d3d1fdea1c757
soasme <soasme@gmail.com>
2016-01-14 10:36:04 +0800

    I forgot to package static into package.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Perm',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/soasme/flask-perm',
     license='MIT',
     author='Ju Lin',
