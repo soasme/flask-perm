761bba48a42a8fec5b05b9b984476a3ff9cdff3e
soasme <soasme@gmail.com>
2016-01-14 09:22:48 +0800

    Filter user group permissions

--- flask_perm/services/user_group_permission.py
+++ flask_perm/services/user_group_permission.py
@@ -21,11 +21,11 @@ def create(user_group_id, permission_id):
         ).first()
     return user_group_permission
 
-def delete(user_group_id, permission_id):
-    user_group_permission = UserGroupPermission.query.filter_by(
-        user_group_id=user_group_id,
-        permission_id=permission_id
-    ).first()
+def get(id):
+    return UserGroupPermission.query.get(id)
+
+def delete(id):
+    user_group_permission = UserGroupPermission.query.get(id)
     if user_group_permission:
         db.session.delete(user_group_permission)
     db.session.commit()
@@ -61,3 +61,18 @@ def get_permissions_by_user_group(user_group_id):
         UserGroupPermission.permission_id
     ).all()
     return [row.permission_id for row in rows]
+
+def filter_user_group_permissions(filter_by, offset, limit, sort_field='created_at', sort_dir='desc'):
+    query = UserGroupPermission.query
+    if filter_by:
+        query = query.filter_by(**filter_by)
+    field = getattr(UserGroupPermission, sort_field)
+    order_by = getattr(field, sort_dir.lower())()
+    return query.order_by(order_by).offset(offset).limit(limit).all()
+
+def rest(user_permission):
+    return dict(
+        id=user_permission.id,
+        user_group_id=user_permission.user_group_id,
+        permission_id=user_permission.permission_id,
+    )
