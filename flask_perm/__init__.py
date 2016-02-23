4b4305cfee499cfc91865f364598d9c73bb8299f
soasme <soasme@gmail.com>
2016-02-23 15:19:39 +0800

    Bump version: 0.1.9 → 0.1.10

--- flask_perm/__init__.py
+++ flask_perm/__init__.py
@@ -3,4 +3,4 @@
 from .app import Perm
 from . import services
 
-__version__ = '0.1.9'
+__version__ = '0.1.10'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Perm',
-    version='0.1.9',
+    version='0.1.10',
     url='https://github.com/soasme/flask-perm',
     license='MIT',
     author='Ju Lin',
