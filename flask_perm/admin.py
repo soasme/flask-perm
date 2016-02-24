22d08aa11216d8ee367f8b4a11a14e08b3917dfd
soasme <soasme@gmail.com>
2016-02-24 16:14:15 +0800

    Use app.extensions to get perm reference.

--- flask_perm/admin.py
+++ flask_perm/admin.py
@@ -6,7 +6,7 @@ bp = Blueprint('perm-admin', __name__, template_folder='templates', static_folde
 
 @bp.route('/')
 def index():
-    if not bp.perm.has_perm_admin_logined():
+    if not current_app.extensions['perm'].has_perm_admin_logined():
         return redirect(url_for('perm-admin.login'))
 
     render_data = {
@@ -22,8 +22,9 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if bp.perm.check_perm_admin_auth(username, password):
-            bp.perm.login_perm_admin()
+        admin_id = current_app.extensions['perm'].get_perm_admin_id_by_auth(username, password)
+        if admin_id:
+            current_app.extensions['perm'].login_perm_admin(admin_id)
             return redirect(url_for('perm-admin.index'))
         else:
             flash(u'Invalid Password', 'error')
@@ -32,5 +33,5 @@ def login():
 
 @bp.route('/logout')
 def logout():
-    bp.perm.logout_perm_admin()
+    current_app.extensions['perm'].logout_perm_admin()
     return redirect(url_for('perm-admin.login'))
