9bd5ae7b5e3565dbd523926a5d972b502ea34e5f
soasme <soasme@gmail.com>
2016-02-24 16:12:18 +0800

    Define super admin model.

---
+++ flask_perm/services/super_admin.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from ..core import db, bcrypt
+from ..models import SuperAdmin
+
+def create(email, password):
+    super_admin = SuperAdmin(
+        email=email,
+        password=bcrypt.generate_password_hash(password)
+    )
+    db.session.add(super_admin)
+    db.session.commit()
+    return super_admin
+
+def to_dict(obj):
+    return dict(
+        id=obj.id,
+        email=obj.email,
+    )
+
+def verify_password(email, password):
+    super_admin = get_by_email(email)
+    if not super_admin:
+        return False
+    if not bcrypt.check_password_hash(super_admin.password, password):
+        return False
+    return True
+
+def delete(id):
+    super_admin = get(id)
+    if super_admin:
+        db.session.delete(super_admin)
+    db.session.commit()
+
+def reset_password(id, password):
+    super_admin = get(id)
+    if super_admin:
+        super_admin.password = bcrypt.generate_password_hash(password)
+        db.session.add(super_admin)
+    db.session.commit()
+
+def get(id):
+    return SuperAdmin.query.get(id)
+
+def get_by_email(email):
+    return SuperAdmin.query.filter_by(email=email).first()
