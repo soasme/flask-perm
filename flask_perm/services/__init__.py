78a421e4c97527072069ad6d20f54dacdfe3634e
soasme <soasme@gmail.com>
2016-02-24 16:12:44 +0800

    Expose SuperAdminService.

--- flask_perm/services/__init__.py
+++ flask_perm/services/__init__.py
@@ -3,6 +3,7 @@
 __all__ = [
     'UserGroupService', 'PermissionService', 'UserGroupMemberService',
     'UserGroupPermissionService', 'VerificationService',
+    'SuperAdminService',
 ]
 
 from . import user_group as UserGroupService
@@ -11,3 +12,4 @@ from . import user_group_member as UserGroupMemberService
 from . import user_permission as UserPermissionService
 from . import user_group_permission as UserGroupPermissionService
 from . import verification as VerificationService
+from . import super_admin as SuperAdminService
