7302746be895a7a9c40a52766e90c92f15f6f82e
soasme <soasme@gmail.com>
2016-02-25 15:16:14 +0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Perm',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/soasme/flask-perm',
     license='MIT',
     author='Ju Lin',
