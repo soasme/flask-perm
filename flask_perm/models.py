5b7d4aa5dd6de53e1dea717d532d41f0123902d0
soasme <soasme@gmail.com>
2016-02-25 15:15:02 +0800

    Extend superadmin password length.

--- flask_perm/models.py
+++ flask_perm/models.py
@@ -12,8 +12,8 @@ class SuperAdmin(db.Model):
     )
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), nullable=False)
-    password = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
 class Permission(db.Model):
