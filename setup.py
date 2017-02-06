4ac11d935fd2bfc8e6b3b01e615852cf34c7ffe3
zhaodanhua <danhua.zhao@liwushuo.com>
2017-02-06 14:26:00 +0800

    0.2.6 -> 0.2.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Perm',
-    version='0.2.6',
+    version='0.2.7',
     url='https://github.com/soasme/flask-perm',
     license='MIT',
     author='Ju Lin',
